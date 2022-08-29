ef201541349ff7ea9700290bac3ce49a3788916d
Cosimo Giovanni Negri <cosimogiovanninegri@gmail.com>
2022-08-29 22:54:13 +0200

    __init__ file to handle imports

---
+++ src/python/datastructures/__init__.py
@@ -0,0 +1,5 @@
+from .linkedlist.doubly_linked_list import DoublyLinkedList
+from .queue.linked_queue import LinkedQueue
+from .stack.linked_stack import LinkedStack
+from .hashtable.hash_table_separate_chaining import HashTableSeparateChaining
+from .set.set import Set
--- src/python/datastructures/linkedlist/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-if __package__:
-    from .doubly_linked_list import DoublyLinkedList
-else:
-    sys.path.append(os.path.dirname(__file__))
-    from doubly_linked_list import DoublyLinkedList
